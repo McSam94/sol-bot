6173be2fb3c463177910d024368096c4d1e26d91
McSam <mcsam1216@gmail.com>
2022-07-08 02:33:06 +0800

    chore: update favicon and meta

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Image from 'next/image';
 import { Wallet } from './common';
 import RunPanel from './pages/run-panel';
 
@@ -6,7 +7,10 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
 	return (
 		<div className='flex flex-col max-w-full min-h-screen'>
 			<div className='flex flex-row items-center bg-black justify-between w-full px-10 h-20 shadow-md text-white'>
-				<div className='text-xl font-bold'>Jupiter Bot</div>
+				<div className='flex flex-row items-center space-x-2'>
+					<Image src='/logo.svg' alt='logo' width={30} height={30} />
+					<div className='text-xl font-bold'>Jupiter Bot</div>
+				</div>
 				<Wallet />
 			</div>
 			<div className='flex flex-row' style={{ height: 'calc(100vh - 80px)' }}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-	return <div id='blocklyDiv' className='h-full w-full'></div>;
+	return (
+		<>
+			<Head>
+				<title>Jupiter Bot</title>
+				<meta name='description' content='Automate solana exchange without programming.' />
+			</Head>
+			<div id='blocklyDiv' className='h-full w-full'></div>
+		</>
+	);
 };
 
 export default Home;
